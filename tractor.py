93888c2addeeb24ae05cd0430f372282184c5e3e
dardwo <dardwo@st.amu.edu.pl>
2023-03-11 12:14:30 +0100

    Added tractor rotation

--- tractor.py
+++ tractor.py
@@ -1,3 +1,5 @@
+import pygame
+
 class Tractor:
     #this is where tractor spawns when program starts (center)
     x=500
@@ -7,3 +9,11 @@ class Tractor:
     #it's size
     width = 20
     height = 20
+    
+    #tractor image rotation
+    ROTATION_IMG = pygame.image.load('assets/tractor/tractor_UP.png')
+    UP = pygame.image.load('assets/tractor/tractor_UP.png')
+    DOWN = pygame.image.load('assets/tractor/tractor_DOWN.png')
+    LEFT = pygame.image.load('assets/tractor/tractor_LEFT.png')
+    RIGHT = pygame.image.load('assets/tractor/tractor_RIGHT.png')
+    
