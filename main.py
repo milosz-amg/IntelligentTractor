b869a5ade3c82a4f24e447413b3644dce2acbe18
JakubPaszke <kurczakjakub@gmail.com>
2023-03-25 12:33:24 +0100

    Changing the way that farm is generating first corps on all fields

--- main.py
+++ main.py
@@ -1,35 +1,54 @@
 import pygame
 import sys
-import tractor
-import src.screen as screen
-import src.plant as plant
-from src.Tractor import Tractor as ractor2
-#import src.tractor as tractor2
+import random
+from settings import screen_height, screen_width, SIZE, SPECIES, block_size, tile, road_coords
+from src.map import drawRoads, seedForFirstTime
+from src.Tractor import Tractor
+from src.Plant import Plant
 
 # pygame initialization
 pygame.init()
+clock = pygame.time.Clock()
 pygame.mouse.set_visible(False)
 
-# #new tractor sprite - todo
-# tr=tractor2('oil','manual',36,36)
-# tr_group = pygame.sprite.Group()
-# tr_group.add()
+#GAME SCREEN
+screen = pygame.display.set_mode(SIZE)
+pygame.display.set_caption("Traktor_interaktor")
+background = pygame.image.load("assets/farmland.jpg")
+background = pygame.transform.scale(background,SIZE)
+screen.fill((90,50,20))
+background.fill((90,50,20))
+background = drawRoads(background)
 
-# creating agent
-myTractor = tractor.Tractor
+for line in range(26):
+            pygame.draw.line(background, (0, 0, 0), (0, line * 36), (SIZE[0], line * 36))
+            pygame.draw.line(background, (0, 0, 0), (line * 36, 0), (line * 36, SIZE[1]))
+
+#TRACTOR
+tractor = Tractor('oil','manual')
+tractor_group = pygame.sprite.Group()
+tractor_group.add(tractor)
+
+#PLANTS
+plant_group = pygame.sprite.Group()
+plant_group = seedForFirstTime()
+
+if __name__ == "__main__":
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                 tractor.collect(plant_group)
+            
+        pygame.display.flip()
+        screen.blit(background,(0,0))
+        plant_group.draw(screen)
+        tractor_group.draw(screen)
+        tractor_group.update()
+        clock.tick(60)
         
-#if __name__ == "__main__":
-running = True
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            pygame.quit()
-            sys.exit()
-        # defines agent movement
-        tractor.movement(myTractor)
-    # screen visualisation
-    screen.set_screen(myTractor)
-    
-#pygame.quit()
