a88cb60fbd4cd9018bcc8cff5a90adff863dad50
dardwo <dardwo@st.amu.edu.pl>
2023-03-26 12:00:34 +0200

    fix tractor movement, added function to tractor and plant

--- src/Plant.py
+++ src/Plant.py
@@ -9,24 +9,36 @@ class Plant(pygame.sprite.Sprite):
         if species=="carrot":
             self.growth_time=100
             self.weight=50
+            self.min_hydration = 30
+            self.max_hydration = 60
+            self.soil_type = "torf"
             self.fertilizer="carrot_fertilizer"
             self.pic_path="assets/Carrot.png"
         
         elif species=="beetroot":
             self.growth_time=200
             self.weight=200
+            self.min_hydration = 20
+            self.max_hydration = 60
+            self.soil_type = "piaszczyste"
             self.fertilizer="beetroot_fertilizer"
             self.pic_path="assets/Beetroot.png"
 
         elif species=="potato":
             self.growth_time=100
             self.weight=100
+            self.min_hydration = 10
+            self.max_hydration = 30
+            self.soil_type = "ilaste"
             self.fertilizer="potatoe_fertilizer"
             self.pic_path="assets/Potato.png"
 
         else:
             self.growth_time=250
             self.weight=75
+            self.min_hydration = 10
+            self.max_hydration = 65
+            self.soil_type = "gliniaste"
             self.fertilizer="wheat_fertilizer"
             self.pic_path="assets/Wheat.png"
 
