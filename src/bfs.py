560cceafc929766ac72cc23f608978ba4417c751
dardwo <dardwo@st.amu.edu.pl>
2023-04-22 21:43:28 +0200

    added successor

--- src/bfs.py
+++ src/bfs.py
@@ -13,7 +13,23 @@ class BFS:
         self.explored = [] 
     
     def successor(self, state):
-        pass
+        pos_x, pos_y, rotation = state
+        options = []
+        
+        if rotation == directions[0]:
+            states = [(pos_x, pos_y - block_size, directions[0]), (pos_x, pos_y, directions[270]), (pos_x, pos_y, directions[90])]
+            actions = ['F', 'L', 'R']
+        elif rotation == directions[90]:
+            states = [(pos_x + block_size, pos_y, directions[90]), (pos_x, pos_y, directions[0]), (pos_x, pos_y, directions[180])]
+            actions = ['F', 'L', 'R']
+        elif rotation == directions[180]:
+            states = [(pos_x, pos_y + block_size, directions[180]), (pos_x, pos_y, directions[90]), (pos_x, pos_y, directions[270])]
+            actions = ['F', 'L', 'R']
+        elif rotation == directions[270]:
+            states = [(pos_x - block_size, pos_y, directions[270]), (pos_x, pos_y, directions[0]), (pos_x, pos_y, directions[180])]
+            actions = ['F', 'L', 'R']
+            
+        return states, actions
         
     def search(self):
         pass
